2bf4f32797aa6a49de7dbf9c6d5464f5d2593812
Polina <polinawakeup@yandex.ru>
2024-12-12 20:56:37 +0300

    Внесены изменения в первое задание с учетом комментариев

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let x;
-  let anotherX;
   let d = Math.pow(b, 2) - 4 * a * c;
-  if (d < 0) {
-    console.log('У уравнения нет корней, дискриминант меньше нуля')
-    return arr;
-  } else if (d === 0) {
-    x = -b / (a * 2);
+  if (d === 0) {
+    let x = -b / (a * 2);
     arr.push(x);
-  } else {
-    x = (-b + Math.sqrt(d) ) / (2 * a);
-    anotherX = (-b - Math.sqrt(d) ) / (2 * a);
+  } else if (d > 0) {
+    let x = (-b + Math.sqrt(d) ) / (2 * a);
+    let anotherX = (-b - Math.sqrt(d) ) / (2 * a);
     arr.push(x, anotherX);
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent / 100 / 12;
+  const percentRate = percent / 100 / 12;
   let body = amount - contribution;
-  let payment = body * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
-  let totalAmount = parseFloat((payment * countMonths).toFixed(2));
-  return totalAmount;
+  let payment = body * (percentRate + (percentRate / (((1 + percentRate) ** countMonths) - 1)));
+  return parseFloat((payment * countMonths).toFixed(2));
 }
