4f2c1c3e72b6844f0e8e5d36a0486d590f49cc06
Polina <polinawakeup@yandex.ru>
2025-01-21 02:01:29 +0300

    Homework exception-closure completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(value){
+    let amount = parseFloat(value);
+    if (!amount){
+        throw new Error("Невалидное значение");
+    };
+    return amount;
+};
+
+function validateCount(value){
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    };
+};
+
+class Triangle {
+    constructor(a, b, c){
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c
+        };
+    };
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    };
+
+    get area(){
+        let p = this.perimeter / 2;
+        return parseFloat((Math.sqrt( p * (p - this.a) * (p - this.b) * (p - this.c) )).toFixed(3));
+    };
+};
+
+function getTriangle(a, b, c){
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+
+            get perimeter(){
+                return "Ошибка! Треугольник не существует";
+            },
+
+            get area(){
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
